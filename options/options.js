baf762928d70ad4bdc57953fc32b366cc09afaff
arunk <arun.kumar@crowdbotics.com>
2023-03-31 12:01:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fourthmobileapp-5",
+  url: "https://fourthmobileapp-5.botics.co",
+  api: "https://fourthmobileapp-5.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
